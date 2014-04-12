2cbfe99f5c1ead047d3f2ce897166a3234e02a81
Tatsuya Ogi <t.oginogin@gmail.com>
2014-04-12 17:49:14 +0900

    クラス化第一弾

--- konkatsu.rb
+++ konkatsu.rb
@@ -1,32 +1,88 @@
-# 女性側の希望に含まれているかチェック
-def love_match(lovers, man, key)
-  lover = lovers[man][key]
-  if lover
-    if lovers[lover].has_value?(man)
-      p "#{man}-#{lover}" 
-      return true 
-    end
+class Lovers
+  attr_accessor :man, :woman
+  def initialize(init_man, init_woman)
+    @man = init_man
+    @woman = init_woman
   end
-  false 
+
+  def to_s
+    "#{man}-#{woman}"
+  end
 end
 
-# 入力パターン解析
-def input_parse(lovers, input_string)
-  input_string.split("\n").each do |lover|
-    /(\w):(.+)/.match(lover)
-    name = $1
-    lovers[name] = {}
-    ($2 || "").split(",").each_with_index do |s, i|
-      lovers[name][:first] = s if i == 0
-      lovers[name][:second] = s if i == 1
-      lovers[name][:third] = s if i == 2
+class LoversPattern
+  # 入力パターン解析
+  def self.input_parse(input_string)
+    lovers_pattern = {}
+    input_string.split("\n").each do |lover|
+      /(\w):(.+)/.match(lover)
+      name = $1
+      lovers_pattern[name] = []
+      ($2 || "").split(",").each {|s| lovers_pattern[name] << s}
     end
+    lovers_pattern
+  end
+end
+
+class Konkatsu
+  attr_accessor :lovers_pattern, :lovers
+
+  def initialize
+    @lovers_pattern = {}
+    @lovers = []
+  end
+
+  # 女性側の希望に含まれているかチェック
+  def love_match(man, lank)
+    return nil unless lovers_pattern[man]
+
+    if lover = lovers_pattern[man][lank]
+      if lovers_pattern[lover].include?(man)
+        return Lovers.new(man, lover)
+      end
+    end
+    nil
+  end
+
+
+  def execute( pattern )
+    # 希望リスト解析
+    @lovers_pattern = LoversPattern.input_parse pattern
+
+    # 男性の希望をチェック
+    lovers_pattern.each_key do |man|
+      next unless /[A-Z]+/.match(man)
+
+      # 1st希望同士のチェック
+      if first = lovers_pattern[man].first
+        if woman_first = lovers_pattern[first].first
+          if man == woman_first
+            lovers << Lovers.new(man, first)
+            next
+          end
+        end
+      end
+
+      # 希望に含まれているかを第一希望からチェック
+      # ToDo:第n希望まであるかはLoversPatternに判断させたい
+      if lover = love_match(man, 0)
+        lovers << lover and next
+      end
+      if lover = love_match(man, 1)
+        lovers << lover and next
+      end
+      if lover = love_match(man, 2)
+        lovers << lover and next
+      end
+    end
+
+    lovers
   end
 end
 
 # 希望リスト
 =begin
-lovers_string = <<LOVERS
+pattern = <<LOVERS
 A:c,b,a
 B:a,b,d
 C:a,c,b
@@ -37,7 +93,7 @@ c:B,A,C
 d:D,C,A
 LOVERS
 =end
-lovers_string = <<LOVERS
+pattern = <<LOVERS
 A:c,a,b
 B:c,f,a
 C:f,c,b
@@ -52,30 +108,7 @@ e:C,A,E
 f:D,B,A
 LOVERS
 
-# 希望リスト解析
-lovers = {}
-input_parse lovers, lovers_string
-
-# 男性の希望をチェック
-lovers.each_key do |man|
-  next unless /[A-Z]+/.match(man)
-
-  # 1st希望同士のチェック
-  first = lovers[man][:first]
-  if first
-    woman_first = lovers[first][:first]
-    if woman_first
-      if man == woman_first
-        p "#{man}-#{first}" 
-        next
-      end
-    end
-  end
-
-  # 希望に含まれているかを第一希望からチェック
-  next if love_match lovers, man, :first
-  next if love_match lovers, man, :second
-  next if love_match lovers, man, :third
-
-end
+konkatsu = Konkatsu.new
+lovers = konkatsu.execute pattern
+lovers.each {|lover| puts lover}
 
