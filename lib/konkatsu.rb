b96854583c492c166e49246c14f61f01e9c4c598
Tatsuya Ogi <t.oginogin@gmail.com>
2014-04-13 09:05:28 +0900

    ロジック変更

--- lib/konkatsu.rb
+++ lib/konkatsu.rb
@@ -1,84 +1,94 @@
 class Lovers
-  attr_accessor :man, :woman
-  def initialize(init_man, init_woman)
-    @man = init_man
-    @woman = init_woman
+  attr_accessor :from, :to, :point, :is_merged
+  def initialize(init_from, init_to, init_point)
+    @from = init_from
+    @to = init_to
+    @point = init_point
+    @is_merged = false
   end
 
   def to_s
-    "#{man}-#{woman}"
+    "#{from}-#{to}"
+  end
+
+  def man?
+    from =~ /[A-Z]+/
+  end
+
+  def merged?
+    is_merged
   end
 end
 
 class LoversPattern
+  attr_accessor :lovers
+
+  def initialize
+    @lovers = []
+  end
+
   # 入力パターン解析
-  def self.input_parse(input_string)
-    lovers_pattern = {}
-    input_string.split("\n").each do |lover|
-      /(\w):(.+)/.match(lover)
+  def parse(input_string)
+    @lovers = []
+    input_string.each_line do |lover_list|
+      /(\w):(.+)/.match(lover_list)
       name = $1
-      lovers_pattern[name] = []
-      ($2 || "").split(",").each {|s| lovers_pattern[name] << s}
+      ($2 || "").split(",").each_with_index do |lover, i|
+        lovers << Lovers.new(name, lover, i+1)
+      end
     end
-    lovers_pattern
+  end
+
+  def search(from, to)
+    lists = []
+    lovers.each do |lover|
+      lists << lover if lover.from == from && lover.to == to
+    end
+    lists
+  end
+
+  # 女性の希望ポイントを男性の希望ポイントにマージする
+  def merge_point
+    lovers.each do |lover|
+      unless lover.man?
+        men = search(lover.to, lover.from)
+        men.each {|man| man.point += lover.point; man.is_merged = true}
+      end
+    end
+  end
+
+  # 女性から希望されてポイント加算された男性のみ抽出
+  def merged_lovers
+    # 女性から希望された男性のみ抽出し、ポイントの若い順に並び替え
+    sorted_lovers = []
+    lovers.each {|lover| sorted_lovers << lover if lover.merged?}
+    sorted_lovers.sort! {|a, b| a.point <=> b.point}
+
+    # すでにペアになった人を除外
+    coupled_names = []
+    each_lovers = []
+    sorted_lovers.each do |lover|
+      is_coupled = false
+      coupled_names.each {|name| is_coupled = true if lover.from == name || lover.to == name}
+      next if is_coupled
+      coupled_names << lover.from
+      coupled_names << lover.to
+      each_lovers << lover
+    end
+
+    # 名前順に並び替え
+    each_lovers.sort {|a, b| a.from <=> b.from}
   end
 end
 
 class Konkatsu
-  attr_accessor :lovers_pattern, :lovers
-
-  def initialize
-    @lovers_pattern = {}
-    @lovers = []
-  end
-
-  # 女性側の希望に含まれているかチェック
-  def love_match(man, lank)
-    return nil unless lovers_pattern[man]
-
-    if lover = lovers_pattern[man][lank]
-      if lovers_pattern[lover].include?(man)
-        return Lovers.new(man, lover)
-      end
-    end
-    nil
-  end
-
-
   def execute( pattern )
     initialize
 
-    # 希望リスト解析
-    @lovers_pattern = LoversPattern.input_parse pattern
-
-    # 男性の希望をチェック
-    lovers_pattern.each_key do |man|
-      next unless /[A-Z]+/.match(man)
-
-      # 1st希望同士のチェック
-      if first = lovers_pattern[man].first
-        if woman_first = lovers_pattern[first].first
-          if man == woman_first
-            lovers << Lovers.new(man, first)
-            next
-          end
-        end
-      end
-
-      # 希望に含まれているかを第一希望からチェック
-      # ToDo:第n希望まであるかはLoversPatternに判断させたい
-      if lover = love_match(man, 0)
-        lovers << lover and next
-      end
-      if lover = love_match(man, 1)
-        lovers << lover and next
-      end
-      if lover = love_match(man, 2)
-        lovers << lover and next
-      end
-    end
-
-    lovers
+    lovers_pattern = LoversPattern.new
+    lovers_pattern.parse pattern
+    lovers_pattern.merge_point
+    lovers_pattern.merged_lovers
   end
 end
 
