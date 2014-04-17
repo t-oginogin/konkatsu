b5687726635becd93f0626c0b9395ba6917ac7ff
Junichi Ito <jit@sonicgarden.jp>
2014-04-17 09:26:39 +0900

    Remove useless '+1'.

--- lib/konkatsu.rb
+++ lib/konkatsu.rb
@@ -22,7 +22,7 @@ class PairsPattern
     input_string.each_line.map do |pair_list|
       /(?<name>\w):(?<others>.+)/ =~ pair_list
       (others || "").split(",").each_with_index.map do |pair, i|
-        Pair.new(name, pair, i+1)
+        Pair.new(name, pair, i)
       end
     end.flatten
   end
