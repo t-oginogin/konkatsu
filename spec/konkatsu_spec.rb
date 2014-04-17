a895c5f07935fefba062fdbbe5764a9aeb634b79
Junichi Ito <jit@sonicgarden.jp>
2014-04-17 10:38:29 +0900

    Refactor spec.

--- spec/konkatsu_spec.rb
+++ spec/konkatsu_spec.rb
@@ -2,45 +2,42 @@ require 'konkatsu'
 
 describe Konkatsu do
   describe 'execute' do
+    subject{ Konkatsu.execute(pattern).join(?,) }
     context 'pattern1' do
-      it 'returns pairs' do
-        # 希望リスト
-        pattern = <<-LOVERS
-        A:c,b,a
-        B:a,b,d
-        C:a,c,b
-        D:d,a,c
-        a:A,C,D
-        b:D,A,B
-        c:B,A,C
-        d:D,C,A
+      # 希望リスト
+      let(:pattern) {
+        <<-LOVERS
+          A:c,b,a
+          B:a,b,d
+          C:a,c,b
+          D:d,a,c
+          a:A,C,D
+          b:D,A,B
+          c:B,A,C
+          d:D,C,A
         LOVERS
-
-        lovers = Konkatsu.execute pattern
-        expect(lovers.join).to eq("A-cB-bC-aD-d")
-      end
+      }
+      it { should eq "A-c,B-b,C-a,D-d" }
     end
     context 'pattern2' do
-      it 'returns pairs' do
-        # 希望リスト
-        pattern = <<-LOVERS
-        A:c,a,b
-        B:c,f,a
-        C:f,c,b
-        D:d,d,d
-        E:
-        F:e,c,a
-        a:A,D,F
-        b:C,B,A
-        c:D,A,C
-        d:A,A,B
-        e:C,A,E
-        f:D,B,A
+      # 希望リスト
+      let(:pattern) {
+        <<-LOVERS
+          A:c,a,b
+          B:c,f,a
+          C:f,c,b
+          D:d,d,d
+          E:
+          F:e,c,a
+          a:A,D,F
+          b:C,B,A
+          c:D,A,C
+          d:A,A,B
+          e:C,A,E
+          f:D,B,A
         LOVERS
-
-        lovers = Konkatsu.execute pattern
-        expect(lovers.join).to eq("A-aB-fC-b")
-      end
+      }
+      it { should eq "A-a,B-f,C-b" }
     end
   end
 end
