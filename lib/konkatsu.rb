e5f812049b64dbd9f6549ecd64da76cf2d647860
Junichi Ito <jit@sonicgarden.jp>
2014-04-17 10:11:37 +0900

    Remove unused accessors.

--- lib/konkatsu.rb
+++ lib/konkatsu.rb
@@ -1,5 +1,6 @@
 class Pair
   attr_accessor :from, :to, :point
+
   def initialize(init_from, init_to, init_point)
     @from = init_from
     @to = init_to
@@ -42,8 +43,6 @@ class PairsPattern
 end
 
 class Konkatsu
-  attr_accessor :all_pairs, :love_pairs
-
   def initialize(pattern)
     @all_pairs = PairsPattern.parse pattern
     @love_pairs = []
@@ -52,11 +51,11 @@ class Konkatsu
   # 女性の希望ポイントを男性の希望ポイントにマージする
   # 女性から希望のあった男性のみ結果用に格納
   def merge_point
-    all_pairs.select(&:woman?).each do |person|
+    @all_pairs.select(&:woman?).each do |person|
       men = search_men_for_this_woman(person)
       men.each do |man|
         man.point += person.point
-        self.love_pairs << man
+        @love_pairs << man
       end
     end
   end
@@ -64,11 +63,11 @@ class Konkatsu
   # 女性から希望されてポイント加算された男性のみ抽出
   def merged_pairs
     # ポイントの若い順に並び替え
-    love_pairs.sort!
+    @love_pairs.sort!
     except_couple
 
     # 名前順に並び替え
-    love_pairs.sort {|a, b| a.from <=> b.from}
+    @love_pairs.sort {|a, b| a.from <=> b.from}
   end
   
   def self.execute( pattern )
@@ -80,13 +79,13 @@ class Konkatsu
   private
 
   def search_men_for_this_woman(woman)
-    all_pairs.select {|man| man.love_each_other?(woman) }
+    @all_pairs.select {|man| man.love_each_other?(woman) }
   end
 
   # 希望順にペアを確定し、すでにペアになった人を除外
   def except_couple
     coupled_pairs = []
-    self.love_pairs = love_pairs.select do |pair|
+    @love_pairs = @love_pairs.select do |pair|
       if coupled_pairs.none? {|coupled_pair| pair.person_exists?(coupled_pair) }
         coupled_pairs << pair
       end
