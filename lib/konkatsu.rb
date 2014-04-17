293e96269f0d3348e16637b102ffe51f4b459b7d
Junichi Ito <jit@sonicgarden.jp>
2014-04-17 10:25:34 +0900

    Merge lines.

--- lib/konkatsu.rb
+++ lib/konkatsu.rb
@@ -50,6 +50,7 @@ class Konkatsu
   def merged_pairs!
     # ポイントの若い順に並び替え
     @love_pairs.sort!
+
     except_couple!
 
     # 名前順に並び替え
@@ -81,9 +82,7 @@ class Konkatsu
   def parse(input_string)
     input_string.each_line.map do |pair_list|
       /(?<name>\w):(?<others>.+)/ =~ pair_list
-      (others || "").split(",").each_with_index.map do |pair, i|
-        Pair.new(name, pair, i)
-      end
+      (others || "").split(",").each_with_index.map {|pair, i| Pair.new(name, pair, i) }
     end.flatten
   end
 end
